457985548cbc88672fe37196fbedf336270bc7ee
Tiago <tiago.ary10@gmail.com>
2022-09-13 01:03:07 -0300

    ajustes api

--- APIProvider.py
+++ APIProvider.py
@@ -1,13 +1,15 @@
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify, request
 import osmnx as ox
 from AppController import AppController
 from utils.JsonUtil import JsonUtil
 from utils.SolverUtil import Solver
-
 app = Flask(__name__)
 
 G = ox.graph_from_xml("map.osm")
 PF = Solver(G)
+algorithms = [PF.BFS, PF.DFS, PF.buscaCustoUniforme,
+              PF.BestFirst, PF.HillClimbing, PF.Aestrela]
 
 
 @app.route('/')
@@ -15,7 +17,20 @@ def home():
     return "Hello, World!"
 
 
-@app.route('/rota')
-def getPath():
-    path = AppController.FindPath(G, 8986661911, 253406715, PF.Aestrela)
+@app.route('/rota/<num>', methods=['GET', 'POST'])
+def getPath(num):
+    req = request.get_json()
+    origem = (req['origem']['x'], req['origem']['y'])
+    destino = (req['destino']['x'], req['destino']['y'])
+    for node in G.nodes:
+        id = node
+        no = G.nodes.get(node)
+        no = (no['x'], no['y'])
+        if origem == no:
+            origem = id
+        if destino == no:
+            destino = id
+    num = int(num)
+    algorithms[num]
+    path = AppController.FindPath(G, origem, destino, algorithms[num])
     return jsonify(JsonUtil.buildJSONPath(G, path))
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from utils.SolverUtil import Solver
 
 G = ox.graph_from_xml("map.osm")
 PF = Solver(G)
-
+print(G.nodes.get(253406715))
 # AppController.plotMap(G)
-path = AppController.FindPath(G, 8986661911, 253406715, PF.Aestrela)
-JsonUtil.buildJSONPath(G, path)
+#path = AppController.FindPath(G, 8986661911, 253406715, PF.Aestrela)
+#JsonUtil.buildJSONPath(G, path)
 #AppController.GenerateTreeFromAlgorithm(G, 8986661911, 265021150, PF.BFS)
